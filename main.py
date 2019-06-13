499242f96a699a4d80a5cd23402acb85a40ca4b9
MikeS-1 <36141900+MikeS-1@users.noreply.github.com>
2019-06-13 16:24:03 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 
 
 if __name__ == "__main__":
-    username = sys.argv[0]
+    username = sys.argv[1]
 
     response = requests.get("https://api.github.com/users/{}/events".format(username))
     events = json.loads(response.content)
